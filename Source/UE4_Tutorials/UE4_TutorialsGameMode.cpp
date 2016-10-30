d69d8b41828ee6f0a85ef0848f02ee01adcb55e3
aytona <christopher.aytona@gmail.com>
2016-10-29 23:59:23 -0400

    init

---
+++ Source/UE4_Tutorials/UE4_TutorialsGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE4_Tutorials.h"
+#include "UE4_TutorialsGameMode.h"
+
+
+
+
---
+++ Source/UE4_Tutorials/UE4_TutorialsGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UE4_TutorialsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_TUTORIALS_API AUE4_TutorialsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
